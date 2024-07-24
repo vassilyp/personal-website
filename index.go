0c12817fb5c0c94277a4b9178c45ffa831d3a356
Vassily P <vasspetrousevitch@gmail.com>
2024-07-24 00:03:27 -0700

    added "minimal" go server code + initial html

--- index.go
+++ index.go
@@ -1,10 +1,31 @@
-package handler
+package main
 
 import (
-  "fmt"
-  "net/http"
+	"fmt"
+	"html/template"
+	"log"
+	"net/http"
+	"regexp"
 )
 
-func Handler(w http.ResponseWriter, r *http.Request) {
-  fmt.Fprintf(w, "<h1>Hello from Go!</h1>")
+var templates = template.Must(template.ParseFiles("page/index.html"))
+var validPath = regexp.MustCompile("^/$")
+
+func mainHandler(w http.ResponseWriter, r *http.Request) {
+	// redirect to main page if given weird URL path
+	m := validPath.FindStringSubmatch(r.URL.Path)
+	if m == nil {
+		http.Redirect(w, r, "/", http.StatusFound)
+	}
+
+	// TODO: keep in mind we can pass along template variables into this!!!
+	templates.Execute(w, "page/index.html")
+	return
+}
+
+func main() {
+	http.HandleFunc("/", mainHandler)
+
+	fmt.Println("Serving on localhost:8080")
+	log.Fatal(http.ListenAndServe(":8080", nil))
 }
