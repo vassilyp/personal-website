716e6836e3a4b0e18e310fbf282b532bb01a068b
Vassily P <vasspetrousevitch@gmail.com>
2024-07-24 18:32:21 -0700

    air config + html basic site info

--- index.go
+++ index.go
@@ -2,29 +2,14 @@ package main
 
 import (
 	"fmt"
-	"html/template"
 	"log"
 	"net/http"
-	"regexp"
 )
 
-var templates = template.Must(template.ParseFiles("page/index.html"))
-var validPath = regexp.MustCompile("^/$")
-
-func mainHandler(w http.ResponseWriter, r *http.Request) {
-	// redirect to main page if given weird URL path
-	m := validPath.FindStringSubmatch(r.URL.Path)
-	if m == nil {
-		http.Redirect(w, r, "/", http.StatusFound)
-	}
-
-	// TODO: keep in mind we can pass along template variables into this!!!
-	templates.Execute(w, "page/index.html")
-	return
-}
-
 func main() {
-	http.HandleFunc("/", mainHandler)
+	// http.HandleFunc("/", mainHandler)
+	// serve static files (look inside 'static' directory)
+	http.Handle("/", http.FileServer(http.Dir("./static")))
 
 	fmt.Println("Serving on localhost:8080")
 	log.Fatal(http.ListenAndServe(":8080", nil))
