aa58922428324b47611016c6c9820d846999dfc2
Vassily P <vasspetrousevitch@gmail.com>
2024-07-27 00:33:44 -0700

    trying out htmx with go

--- index.go
+++ index.go
@@ -6,10 +6,15 @@ import (
 	"net/http"
 )
 
+func helloHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "<p>world</p>")
+}
+
 func main() {
 	// http.HandleFunc("/", mainHandler)
 	// serve static files (look inside 'static' directory)
 	http.Handle("/", http.FileServer(http.Dir("./static")))
+	http.HandleFunc("/hello", helloHandler)
 
 	fmt.Println("Serving on localhost:8080")
 	log.Fatal(http.ListenAndServe(":8080", nil))
