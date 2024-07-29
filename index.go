afb79c68e9244be72aa1eb23a0b28d188e155b4e
Vassily P <vasspetrousevitch@gmail.com>
2024-07-28 22:37:24 -0700

    added html templates and more content

--- index.go
+++ index.go
@@ -1,20 +1,35 @@
 package main
 
 import (
-	"fmt"
+	"html/template"
 	"log"
 	"net/http"
 )
 
-func helloHandler(w http.ResponseWriter, r *http.Request) {
-	fmt.Println("I am here")
-	w.WriteHeader(http.StatusOK)
-	fmt.Fprintf(w, "<p>world</p>")
+var templates = template.Must(template.ParseFiles(
+	"static/templates/experience.html",
+	"static/templates/education.html"))
+
+func renderTemplate(w http.ResponseWriter, templateName string) {
+	err := templates.ExecuteTemplate(w, templateName+".html", nil)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+	}
+}
+
+func experienceHandler(w http.ResponseWriter, r *http.Request) {
+	renderTemplate(w, "experience")
+}
+
+func educationHandler(w http.ResponseWriter, r *http.Request) {
+	renderTemplate(w, "education")
 }
 
 func main() {
 	mux := http.NewServeMux()
-	mux.HandleFunc("GET /hello/", helloHandler)
+
+	mux.HandleFunc("GET /experience/", experienceHandler)
+	mux.HandleFunc("GET /education/", educationHandler)
 
 	// serve front page by default
 	mux.Handle("/", http.FileServer(http.Dir("./static")))
