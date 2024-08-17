5770bbf67a60606b2ab4e18f902c78314bece182
Vassily P <vasspetrousevitch@gmail.com>
2024-08-16 22:47:26 -0700

    keep hightlight on clicked button

--- static/js/effects.js
+++ static/js/effects.js
@@ -1,12 +1,14 @@
 const techs = document.querySelectorAll("#tech img")
+const animationDuration = 1;    // second(s)
 
 for (let i = 0; i < techs.length; i++) {
   techs[i].addEventListener("mouseover", () => {
-    animationDuration = 1;    // second(s)
 
-    techs[i].style.animation = `spinning ${animationDuration}s ease-in-out`;
-    setTimeout(() => {
-      techs[i].style.animation = "none";
-    }, animationDuration * 1000)
+    if (techs[i].style.animation === "") {
+      techs[i].style.animation = `spinning ${animationDuration}s ease-in-out`;
+      setTimeout(() => {
+        techs[i].style.animation = "";
+      }, animationDuration * 1000)
+    }
   })
 }
