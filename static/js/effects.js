863d90d27d870764ff1bb29eb22999732c1f1843
Vassily P <vasspetrousevitch@gmail.com>
2024-08-16 18:36:32 -0700

    figured out cool js animation for tech images

--- static/js/effects.js
+++ static/js/effects.js
@@ -1,1 +1,12 @@
-alert("hello world")
+const techs = document.querySelectorAll("#tech img")
+
+for (let i = 0; i < techs.length; i++) {
+  techs[i].addEventListener("mouseover", () => {
+    animationDuration = 1;    // second(s)
+
+    techs[i].style.animation = `spinning ${animationDuration}s ease-in-out`;
+    setTimeout(() => {
+      techs[i].style.animation = "none";
+    }, animationDuration * 1000)
+  })
+}
